3e9eb597163979589dd2f83549008db197b721a8
Frankslu <1653954675@qq.com>
2026-01-13 18:40:40 +0800

    feat(CustomL1Hint): remove cancel logic when retry

--- src/main/scala/coupledL2/CustomL1Hint.scala
+++ src/main/scala/coupledL2/CustomL1Hint.scala
@@ -33,10 +33,6 @@ class HintQueueEntry(implicit p: Parameters) extends L2Bundle {
 class CustomL1HintIOBundle(implicit p: Parameters) extends L2Bundle {
   // input information
   val s1 = Flipped(ValidIO(new TaskBundle()))
-  val s2 = new L2Bundle {
-      val task  = Flipped(ValidIO(new TaskBundle()))
-      val retry = Input(Bool())
-  }
 
   val s3 = new L2Bundle {
       val task      = Flipped(ValidIO(new TaskBundle()))
@@ -53,10 +49,8 @@ class CustomL1Hint(implicit p: Parameters) extends L2Module {
   val io = IO(new CustomL1HintIOBundle)
 
   val task_s1 = io.s1
-  val task_s2 = io.s2.task
   val task_s3 = io.s3.task
   val mshrReq_s1 = task_s1.bits.mshrTask
-  val mshrReq_s2 = task_s2.bits.mshrTask
   val mshrReq_s3 = task_s3.bits.mshrTask
   val mergeA_s1  = task_s1.bits.mergeA
   val need_mshr_s3 = io.s3.need_mshr
@@ -95,28 +89,6 @@ class CustomL1Hint(implicit p: Parameters) extends L2Module {
     )
   )
 
-  val mshr_GrantDataRetry_s2 = task_s2.valid && mshrReq_s2 &&
-    (isGrantData(task_s2.bits) || isMergeGrantData(task_s2.bits)) && io.s2.retry && task_s2.bits.replTask
-  val mshr_GrantRetry_s2 = task_s2.valid && mshrReq_s2 &&
-    (isGrant(task_s2.bits) || isMergeGrant(task_s2.bits)) && io.s2.retry && task_s2.bits.replTask
-  val mshr_AccessRetry_s2 = task_s2.valid && mshrReq_s2 &&
-    isAccessAckData(task_s2.bits) && io.s2.retry && task_s2.bits.replTask
-  val enq_s2 = Wire(Decoupled(new HintQueueEntry))
-  enq_s2.valid := mshr_GrantRetry_s2 || mshr_GrantDataRetry_s2 || mshr_AccessRetry_s2
-  enq_s2.bits.source := Mux(task_s2.bits.mergeA, task_s2.bits.aMergeTask.sourceId, task_s2.bits.sourceId)
-  enq_s2.bits.isKeyword := Mux(task_s2.bits.mergeA,
-    task_s2.bits.aMergeTask.isKeyword.getOrElse(false.B),
-    task_s2.bits.isKeyword.getOrElse(false.B)
-  )
-  enq_s2.bits.opcode := ParallelPriorityMux(
-    Seq(
-      mshr_GrantDataRetry_s2 -> GrantData,
-      mshr_GrantRetry_s2 -> Grant,
-      mshr_AccessRetry_s2 -> AccessAckData
-    )
-  )
-
-
   // Hint for "chnTask Hit" will fire@s3
   val chn_Grant_s3     = task_s3.valid && !mshrReq_s3 && !need_mshr_s3 && isGrant(task_s3.bits)
   val chn_GrantData_s3 = task_s3.valid && !mshrReq_s3 && !need_mshr_s3 && isGrantData(task_s3.bits)
@@ -137,7 +109,6 @@ class CustomL1Hint(implicit p: Parameters) extends L2Module {
   val hintEntries = mshrsAll
   val hintEntriesWidth = log2Ceil(hintEntries)
   val hintQueue = Module(new Queue(new HintQueueEntry, hintEntries))
-  val hintNeedCancel = Wire(Bool())
 
   // this will have at most 2 entries
   val hint_s1Queue = Module(new Queue(new HintQueueEntry, 4, flow = true))
@@ -147,21 +118,13 @@ class CustomL1Hint(implicit p: Parameters) extends L2Module {
   assert(hint_s1Queue.io.enq.ready, "hint_s1Queue should never be full")
   assert(hintQueue.io.enq.ready || !hintQueue.io.enq.valid, "hintQueue should never be full")
 
-  val hint_s2Queue = Module(new Queue(new HintQueueEntry, 4, flow = true))
-  val s2Deq = hint_s2Queue.io.deq
-  hint_s2Queue.io.enq <> enq_s2
-  hint_s2Queue.io.deq.ready := hintQueue.io.deq.fire && hintQueue.io.deq.bits === s2Deq.bits
-  assert(hint_s2Queue.io.enq.ready, "hint_s2Queue should never be full")
-
   // For AccessAckData that would never go to LSU, insert a dequing bubble for channel delay accuracy
 
   val respWithDataFired = io.l1Hint.fire && io.l1Hint.bits.hasData
   arb(Seq(enq_s3,  hint_s1Queue.io.deq), hintQueue.io.enq, Some("Hint"))
-  hintQueue.io.deq.ready := io.l1Hint.ready && !RegNext(respWithDataFired, false.B) || hintNeedCancel
+  hintQueue.io.deq.ready := io.l1Hint.ready && !RegNext(respWithDataFired, false.B)
 
-  hintNeedCancel := hintQueue.io.deq.valid && s2Deq.valid && hintQueue.io.deq.bits === s2Deq.bits
-
-  io.l1Hint.valid := hintQueue.io.deq.valid && !hintNeedCancel && !RegNext(respWithDataFired, false.B)
+  io.l1Hint.valid := hintQueue.io.deq.valid && !RegNext(respWithDataFired, false.B)
   io.l1Hint.bits.sourceId := hintQueue.io.deq.bits.source
   io.l1Hint.bits.isKeyword := hintQueue.io.deq.bits.isKeyword
   io.l1Hint.bits.hasData := Seq(GrantData, AccessAckData).map(hintQueue.io.deq.bits.opcode === _).reduce(_||_)
